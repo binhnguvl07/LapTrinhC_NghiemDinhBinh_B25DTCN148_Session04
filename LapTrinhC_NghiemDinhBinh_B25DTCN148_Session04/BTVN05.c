637aaa5e17233ed0dcaa1785b16c189eaa9f7afe
ビングー𖦹 <96338235+binhnguvl07@users.noreply.github.com>
2025-10-27 18:32:27 +0700

    Add files via upload

--- LapTrinhC_NghiemDinhBinh_B25DTCN148_Session04/BTVN05.c
+++ LapTrinhC_NghiemDinhBinh_B25DTCN148_Session04/BTVN05.c
@@ -1,33 +1,16 @@
 #include <stdio.h>
 int main() {
-    int dau, cuoi, sodien, tien;
-    printf("Nhap chi so dien dau thang: ");
-    scanf("%d", &dau);
-    printf("Nhap chi so dien cuoi thang: ");
-    scanf("%d", &cuoi);
-    sodien = cuoi - dau;
-    if (0 <= sodien && sodien <50) {
-        tien = sodien * 10000;
-        printf("So dien tieu thu: %d\n", sodien);
-        printf("Tien dien phai tra: %d VND\n", tien);
-    } else if (50 <= sodien && sodien <100) {
-        tien = sodien * 15000;
-        printf("So dien tieu thu: %d\n", sodien);
-        printf("Tien dien phai tra: %d VND\n", tien);
-    } else if (100 <= sodien && sodien < 150) {
-        tien = sodien * 20000;
-        printf("So dien tieu thu: %d\n", sodien);
-        printf("Tien dien phai tra: %d VND\n", tien);
-    } else if (150 <= sodien && sodien <200) {
-        tien = sodien * 25000;
-        printf("So dien tieu thu: %d\n", sodien);
-        printf("Tien dien phai tra: %d VND\n", tien);
-    } else if (sodien >= 200) {
-        tien = sodien * 30000;
-        printf("So dien tieu thu: %d\n", sodien);
-        printf("Tien dien phai tra: %d VND\n", tien);
-    } else {
-        printf("Chi so dien khong hop le.\n");
+    int a, b, c;
+    printf("Nhap so thu 1: ");
+    scanf("%d", &a);
+    printf("Nhap so thu 2: ");
+    scanf("%d", &b);
+    printf("Nhap so thu 3: ");
+    scanf("%d", &c);
+    if (b < c && c < a) {
+        printf("So thu 3 nam trong khoang giua so 1 va so 2.\n");
+    }else {
+        printf("So thu 3 khong nam trong khoang giua so 1 va so 2.\n");
     }
     return 0;
 }
